23342958a0c145853faed3aeb9f7e7514c0cb445
bijh <bijh@uyunsoft.cn>
2018-04-25 14:27:37 +0800

    添加基本的api设置

--- ReduxSource/lib/createStore.js
+++ ReduxSource/lib/createStore.js
@@ -5,8 +5,43 @@
  * @param {*} preloadedState 加载之前的默认store状态树
  * @param {*} enhancer 组合 store creator 的高阶函数
  */
-function createStore(reducer, preloadedState, enhancer) {
+function createStore(reducer, initedState, enhancer) {
+  var curReducer = reducer;
+  var state = initedState;
+  var listeners = [];
 
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    try {
+      curReducer(state, action);
+    } catch(error) {
+      throw new Error('dispatch action get some errors!');
+    }
+  }
+
+  function subscribe(listener) {
+    this.listeners.push(listener);
+    return function unSubscribe() {
+      let offset = this.listeners.indexOf(listener);
+      if ( offset !== -1) {
+        this.listeners.splice(listener, 1);
+      }
+    }
+  }
+
+  function replaceReducers(nextReducer) {
+    curReducer = nextReducer;
+  }
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+    replaceReducers
+  }
 }
 
 module.exports = createStore
